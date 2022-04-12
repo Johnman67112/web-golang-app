645b3f35f4c48f1ee41332eb485400b42b690391
Johnman67112 <joaovitorcucomo@gmail.com>
2022-04-12 10:22:37 -0300

    server and products struct

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"text/template"
+)
+
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Quantity    int
+}
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	products := []Product{
+		{Name: "T-Shirt", Description: "Blue, really pretty", Price: 39, Quantity: 5},
+		{"Sneakers", "Comfortable", 89, 10},
+		{"Headphone", "Very good", 59, 2},
+		{"New Product", "Really cool", 1.99, 1},
+	}
+
+	temp.ExecuteTemplate(w, "Index", products)
 }
