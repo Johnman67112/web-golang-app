400f21aafc3074b8d4ac1be308dfdd012fec0ecc
Johnman67112 <joaovitorcucomo@gmail.com>
2022-04-13 10:25:50 -0300

    update products and finish base project

--- controllers/products.go
+++ controllers/products.go
@@ -12,6 +12,21 @@ import (
 //Template parse
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
+//Parse number fields from insert request
+func productNumParse(price, quantity string) (float64, int) {
+	convertedPrice, err := strconv.ParseFloat(price, 64)
+	if err != nil {
+		log.Println("Error on price conversion")
+	}
+
+	convertedQuantity, err := strconv.Atoi(quantity)
+	if err != nil {
+		log.Println("Error on quantity conversion")
+	}
+
+	return convertedPrice, convertedQuantity
+}
+
 //Builds index page
 func Index(w http.ResponseWriter, r *http.Request) {
 	products := models.GetProducts()
@@ -38,22 +53,39 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", 301)
 }
 
-//Parse number fields from insert request
-func productNumParse(price, quantity string) (float64, int) {
-	convertedPrice, err := strconv.ParseFloat(price, 64)
-	if err != nil {
-		log.Println("Error on price conversion")
-	}
-
-	convertedQuantity, err := strconv.Atoi(quantity)
-	if err != nil {
-		log.Println("Error on quantity conversion")
-	}
-	return convertedPrice, convertedQuantity
-}
-
+//Delete Products
 func Delete(w http.ResponseWriter, r *http.Request) {
 	productId := r.URL.Query().Get("id")
 	models.DeleteProduct(productId)
+	http.Redirect(w, r, "/", 301)
+}
+
+//Edit products
+func Edit(w http.ResponseWriter, r *http.Request) {
+	productId := r.URL.Query().Get("id")
+	product := models.EditProduct(productId)
+
+	temp.ExecuteTemplate(w, "Edit", product)
+}
+
+//Update products
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		sid := r.FormValue("id")
+		name := r.FormValue("name")
+		description := r.FormValue("description")
+
+		sprice := r.FormValue("price")
+		squantity := r.FormValue("quantity")
+		price, quantity := productNumParse(sprice, squantity)
+
+		id, err := strconv.Atoi(sid)
+		if err != nil {
+			log.Println("Error on id type conversion")
+		}
+
+		models.UpdateProduct(id, name, description, price, quantity)
+	}
+
 	http.Redirect(w, r, "/", 301)
 }
--- models/products.go
+++ models/products.go
@@ -14,7 +14,7 @@ func GetProducts() []Product {
 	db := db.DatabaseConect()
 
 	//Database Query
-	producsSelect, err := db.Query("select * from products")
+	producsSelect, err := db.Query("select * from products order by id asc")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -48,6 +48,7 @@ func GetProducts() []Product {
 func SetProduct(name, description string, price float64, quantity int) {
 	db := db.DatabaseConect()
 
+	//Set products on database
 	insertData, err := db.Prepare("insert into products(name, description, price, quantity) values($1, $2, $3, $4)")
 	if err != nil {
 		panic(err.Error())
@@ -61,12 +62,59 @@ func SetProduct(name, description string, price float64, quantity int) {
 func DeleteProduct(id string) {
 	db := db.DatabaseConect()
 
+	//Delete products from database
 	delete, err := db.Prepare("delete from products where id=$1")
 	if err != nil {
 		panic(err.Error())
 	}
 
 	delete.Exec(id)
+
+	defer db.Close()
+}
+
+func EditProduct(id string) Product {
+	db := db.DatabaseConect()
+
+	//Read products on database
+	product, err := db.Query("select * from products where id=$1", id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	baseProduct := Product{}
+
+	for product.Next() {
+		var id, quantity int
+		var name, description string
+		var price float64
+
+		err = product.Scan(&id, &name, &description, &price, &quantity)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		baseProduct.Id = id
+		baseProduct.Name = name
+		baseProduct.Description = description
+		baseProduct.Price = price
+		baseProduct.Quantity = quantity
+	}
+
+	defer db.Close()
+	return baseProduct
+}
+
+func UpdateProduct(id int, name, description string, price float64, quantity int) {
+	db := db.DatabaseConect()
+
+	//Update products on database
+	UpdateProduct, err := db.Prepare("update products set name=$1, description=$2, price=$3, quantity=$4 where id=$5")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	UpdateProduct.Exec(name, description, price, quantity, id)
 
 	defer db.Close()
 }
